8a11fc93cee14cbabd6167fbf575b2757decb713
Omarrrr <omarsteven2398@gmail.com>
2025-05-01 17:10:31 +0300

    add better font to the app

---
+++ lib/Config/themes/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData mainAppTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    useMaterial3: true,
+    fontFamily: 'Lora',
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(fontFamily: 'Lora'),
+      displayMedium: TextStyle(fontFamily: 'Lora'),
+      displaySmall: TextStyle(fontFamily: 'Lora'),
+      headlineLarge: TextStyle(fontFamily: 'Lora'),
+      headlineMedium: TextStyle(fontFamily: 'Lora'),
+      headlineSmall: TextStyle(fontFamily: 'Lora'),
+      titleLarge: TextStyle(fontFamily: 'Lora'),
+      titleMedium: TextStyle(fontFamily: 'Lora'),
+      titleSmall: TextStyle(fontFamily: 'Lora'),
+      bodyLarge: TextStyle(fontFamily: 'Lora'),
+      bodyMedium: TextStyle(fontFamily: 'Lora'),
+      bodySmall: TextStyle(fontFamily: 'Lora'),
+      labelLarge: TextStyle(fontFamily: 'Lora'),
+      labelMedium: TextStyle(fontFamily: 'Lora'),
+      labelSmall: TextStyle(fontFamily: 'Lora'),
+    ),
+  );
+}
